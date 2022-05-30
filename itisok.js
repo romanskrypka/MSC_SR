4931a2a5d01d06bb1345363984f5cf43e96ba0c4
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2022-05-30 07:25:59 +0300

    1.0.0
    Версия 1. относиться только к HTML. Как только добавиться CSS, будет версия 2. С добавлением JavaScript будет версия 3 и т.д.

---
+++ itisok.js
@@ -0,0 +1,30 @@
+
+
+function sum(x, y) {
+    return x + y
+  }
+x = 1;
+y = 3;
+c = x + y;
+console.log(c);
+console.log(c, x);
+console.log(c + x);
+console.log(c / y);
+
+//Напишите функцию sum, которая бы возвращала сумму a и b;
+function sum (a, b) {
+  return a + b
+}
+
+//Напишите функцию multiply, чтобы она возвращала результат умножения a на b;
+function multiply (a, b) {
+  return a * b
+}
+
+let nft = "us project";
+console.log(nft.length);
+
+
+
+
+
