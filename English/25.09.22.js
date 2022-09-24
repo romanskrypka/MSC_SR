9b811eb18aa470d35a29d884a0661cbaffaa335f
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2022-09-24 21:32:31 +0300

    English JS

---
+++ English/25.09.22.js
@@ -0,0 +1,96 @@
+//Перевести фразы
+let trPh_ru01 = "Зарабатывать токены";
+let trPh_en_I_01 = "Earn tokens";
+let trPh_en_01 = "Earn tokens";
+
+let trPh_ru02 = "Получить приглашение";
+let trPh_en_I_02 = "Get invitation";
+let trPh_en_02 = "Get Invitation";
+
+let trPh_ru03 = "Запустить приложение";
+let trPh_en_I_03 = "Launch application";
+let trPh_en_03 = "Run the application";
+
+let trPh_ru04 = "Принять вызов";
+let trPh_en_I_04 = "Take challenge";
+let trPh_en_04 = "Take the Challenge";
+
+let trPh_ru05 = "Часть вопроса";
+let trPh_en_I_05 = "Part of the question";//Кого чего? Родительный падеж.
+let trPh_en_05 = "Part of the question";
+
+let trPh_ru06 = "Приобрести дом";
+let trPh_en_I_06 = "Buy a house";
+let trPh_en_06 = "Buy a house";
+
+let trPh_ru07 = "Встроенный шкаф";
+let trPh_en_I_07 = "Built-in closet";//Тупо скопировал
+let trPh_en_07 = "Built-in closet";
+
+let trPh_ru08 = "Валюта а также прочие активы";
+let trPh_en_I_08 = "Currency as well other assets";
+let trPh_en_08 = "Currency as well others assets";
+
+//Перевести предложения
+let trSe_ru09 = "Будет дождь.";
+let trSe_en_I_09 = "It will to rain";
+let trSe_en_09 = "It's going to rain.";
+
+let trSe_ru10 = "Плохо не знать, как работает машина.";
+let trSe_en_I_10 = "It is bad to not know, how a car work.";
+let trSe_en_10 = "It's bad to not know how a machine works.";
+
+let trSe_ru11 = "Солнечно.";
+let trSe_en_I_11 = "It is sunny.";
+let trSe_en_11 = "Sunny.";
+
+let trSe_ru12 = "Здесь всегда прохладно в январе.";
+let trSe_en_I_12 = "It is here always chilly in January.";//! не правильно употребил слово here
+let trSe_en_12 = "It's always chilly here in January.";
+
+let trSe_ru13 = "На вечеринке было весело.";
+let trSe_en_I_13 = "It was fun on the party.";
+let trSe_en_13 = "It was fun at the party.";
+
+let trSe_ru14 = "Трудно покидать свою страну.";
+let trSe_en_I_14 = "It is difficult to leave your country.";
+let trSe_en_14 = "It's hard to leave your country.";
+
+let trSe_ru15 = "Становится темно.";
+let trSe_en_I_15 = "It is getting dark.";
+let trSe_en_15 = "It is getting dark.";
+
+let trSe_ru16 = "Сейчас 12 часов.";
+let trSe_en_I_16 = "It's now 12 o'clock.";
+let trSe_en_16 = "It's now 12 o'clock.";
+
+let trSe_ru17 = "Будет сложно запустить этот проект.";
+let trSe_en_I_17 = "It will be launch difficult this project.";
+let trSe_en_17 = "It's going to be hard to get this project up and running.";
+
+let trSe_ru18 = "Важно помнить эти правила.";
+let trSe_en_I_18 = "It is important to remember these rules.";
+let trSe_en_18 = "It is important to remember these rules.";
+
+let trSe_ru19 = "Пора делать домашнюю работу.";
+let trSe_en_I_19 = "It's time to do your homework.";
+let trSe_en_19 = "It's time to do your homework.";
+
+let trSe_ru20 = "На прошлой неделе шел сильный снег.";
+let trSe_en_I_20 = "It snowed heavily last week.";
+let trSe_en_20 = "It snowed heavily last week.";
+
+let trSe_ru21 = "Уже утро.";
+let trSe_en_I_21 = "It is morning already.";
+let trSe_en_21 = "It's morning already.";
+
+let trSe_ru22 = "Эту статью легко читать.";
+let trSe_en_I_22 = "There article easy to read.";
+let trSe_en_22 = "This article is easy to read.";
+
+let trSe_ru23 = "Невозможно в это поверить!";
+let trSe_en_I_23 = "This it is not to believe!";
+let trSe_en_23 = "Impossible this believe it!";
+
+
+
---
+++ English/dist/25.09.22.dev.js
@@ -0,0 +1,76 @@
+"use strict";
+
+//Перевести фразы
+var trPh_ru01 = "Зарабатывать токены";
+var trPh_en_I_01 = "Earn tokens";
+var trPh_en_01 = "Earn tokens";
+var trPh_ru02 = "Получить приглашение";
+var trPh_en_I_02 = "Get invitation";
+var trPh_en_02 = "Get Invitation";
+var trPh_ru03 = "Запустить приложение";
+var trPh_en_I_03 = "Launch application";
+var trPh_en_03 = "Run the application";
+var trPh_ru04 = "Принять вызов";
+var trPh_en_I_04 = "Take challenge";
+var trPh_en_04 = "Take the Challenge";
+var trPh_ru05 = "Часть вопроса";
+var trPh_en_I_05 = "Part of the question"; //Кого чего? Родительный падеж.
+
+var trPh_en_05 = "Part of the question";
+var trPh_ru06 = "Приобрести дом";
+var trPh_en_I_06 = "Buy a house";
+var trPh_en_06 = "Buy a house";
+var trPh_ru07 = "Встроенный шкаф";
+var trPh_en_I_07 = "Built-in closet"; //Тупо скопировал
+
+var trPh_en_07 = "Built-in closet";
+var trPh_ru08 = "Валюта а также прочие активы";
+var trPh_en_I_08 = "Currency as well other assets";
+var trPh_en_08 = "Currency as well others assets"; //Перевести предложения
+
+var trSe_ru09 = "Будет дождь.";
+var trSe_en_I_09 = "It will to rain";
+var trSe_en_09 = "It's going to rain.";
+var trSe_ru10 = "Плохо не знать, как работает машина.";
+var trSe_en_I_10 = "It is bad to not know, how a car work.";
+var trSe_en_10 = "It's bad to not know how a machine works.";
+var trSe_ru11 = "Солнечно.";
+var trSe_en_I_11 = "It is sunny.";
+var trSe_en_11 = "Sunny.";
+var trSe_ru12 = "Здесь всегда прохладно в январе.";
+var trSe_en_I_12 = "It is here always chilly in January."; //! не правильно употребил слово here
+
+var trSe_en_12 = "It's always chilly here in January.";
+var trSe_ru13 = "На вечеринке было весело.";
+var trSe_en_I_13 = "It was fun on the party.";
+var trSe_en_13 = "It was fun at the party.";
+var trSe_ru14 = "Трудно покидать свою страну.";
+var trSe_en_I_14 = "It is difficult to leave your country.";
+var trSe_en_14 = "It's hard to leave your country.";
+var trSe_ru15 = "Становится темно.";
+var trSe_en_I_15 = "It is getting dark.";
+var trSe_en_15 = "It is getting dark.";
+var trSe_ru16 = "Сейчас 12 часов.";
+var trSe_en_I_16 = "It's now 12 o'clock.";
+var trSe_en_16 = "It's now 12 o'clock.";
+var trSe_ru17 = "Будет сложно запустить этот проект.";
+var trSe_en_I_17 = "It will be launch difficult this project.";
+var trSe_en_17 = "It's going to be hard to get this project up and running.";
+var trSe_ru18 = "Важно помнить эти правила.";
+var trSe_en_I_18 = "It is important to remember these rules.";
+var trSe_en_18 = "It is important to remember these rules.";
+var trSe_ru19 = "Пора делать домашнюю работу.";
+var trSe_en_I_19 = "It's time to do your homework.";
+var trSe_en_19 = "It's time to do your homework.";
+var trSe_ru20 = "На прошлой неделе шел сильный снег.";
+var trSe_en_I_20 = "It snowed heavily last week.";
+var trSe_en_20 = "It snowed heavily last week.";
+var trSe_ru21 = "Уже утро.";
+var trSe_en_I_21 = "It is morning already.";
+var trSe_en_21 = "It's morning already.";
+var trSe_ru22 = "Эту статью легко читать.";
+var trSe_en_I_22 = "There article easy to read.";
+var trSe_en_22 = "This article is easy to read.";
+var trSe_ru23 = "Невозможно в это поверить!";
+var trSe_en_I_23 = "This it is not to believe!";
+var trSe_en_23 = "Impossible this believe it!";
