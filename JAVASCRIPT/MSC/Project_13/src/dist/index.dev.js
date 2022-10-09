04892f3faf9d4e4bf65cdc81a3a65e0c97c53bdf
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2022-10-09 10:05:57 +0300

    Project_13_JS_02

--- JAVASCRIPT/MSC/Project_13/src/dist/index.dev.js
+++ JAVASCRIPT/MSC/Project_13/src/dist/index.dev.js
@@ -2,6 +2,7 @@
 
 //import { renderTableRows } from "./prices.js"
 //import "./styles.css"
+var data1 = [["Bitcoin1", "47311"], ["Ethereum1", "3407"], ["Solana1", "163"], ["Tether1", "1"]];
 var data = [["Bitcoin", "47311"], ["Ethereum", "3407"], ["Solana", "163"], ["Tether", "1"]];
 /* Для каждого ряда, полученного в параметре rows, функция renderTableRows должна возвращать следующий HTML:
 <tr>
@@ -14,21 +15,26 @@ function renderTableRows(rows) {
   console.log(rows);
   var xxx = "<tr>\n      <td>\u043D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u043A\u0440\u0438\u043F\u0442\u043E\u0432\u0430\u043B\u044E\u0442\u044B \u0437\u0434\u0435\u0441\u044C</td>\n      <td>\u0441\u0442\u043E\u0438\u043C\u043E\u0441\u0442\u044C \u043A\u0440\u0438\u043F\u0442\u043E\u0432\u0430\u043B\u044E\u0442\u044B \u0437\u0434\u0435\u0441\u044C</td>\n   </tr>";
   rows.forEach(function (row) {
-    xxx += "<tr>\n         <td>".concat(data[0 + 1][0], "</td>\n         <td>").concat(data[0 + 1][1], "</td>\n      </tr>");
+    console.log(row);
+    xxx += "<tr>\n         <td>".concat(row[0], "</td>\n         <td>").concat(row[1], "</td>\n      </tr>");
   }); //нужно создать массив row?
 
   return xxx; // Пишите код решения здесь
-}
+} //?Пример из другого проекта
 
-function getDropdown(currencies) {
-  console.log(currencies);
-  var value = '<option value="">Выберите валюту</option>';
-  currencies.forEach(function (currency) {
-    value += "<option value=\"".concat(currency, "\">").concat(currency, "</option>");
-  });
-  return value; // Пишите код решения здесь
-}
+/*function getDropdown(currencies) {
+   console.log(currencies);
+   let value = '<option value="">Выберите валюту</option>';
+   currencies.forEach(function(currency) {
+      value += `<option value="${currency}">${currency}</option>`;
+   })
+   return value;// Пишите код решения здесь
+}*/
+
 
 var html = renderTableRows(data);
+var html1 = renderTableRows(data1);
 var tbody = document.querySelector("#prices-table tbody");
 tbody.insertAdjacentHTML("beforeend", html);
+var tbody1 = document.querySelector("#prices-table tbody");
+tbody1.insertAdjacentHTML("beforeend", html1);
--- JAVASCRIPT/MSC/Project_13/src/index.js
+++ JAVASCRIPT/MSC/Project_13/src/index.js
@@ -2,12 +2,19 @@
 //import { renderTableRows } from "./prices.js"
 //import "./styles.css"
 
+const data1 = [
+   ["Bitcoin1", "47311"],
+   ["Ethereum1", "3407"],
+   ["Solana1", "163"],
+   ["Tether1", "1"]
+ ];
+
 const data = [
   ["Bitcoin", "47311"],
   ["Ethereum", "3407"],
   ["Solana", "163"],
   ["Tether", "1"]
-]
+];
 
 /* Для каждого ряда, полученного в параметре rows, функция renderTableRows должна возвращать следующий HTML:
 <tr>
@@ -23,31 +30,36 @@ function renderTableRows(rows) {
       <td>стоимость криптовалюты здесь</td>
    </tr>`;
    rows.forEach(function(row) {
+      console.log(row)
       xxx +=`<tr>
-         <td>${data [0+1][0]}</td>
-         <td>${data [0+1][1]}</td>
+         <td>${row [0]}</td>
+         <td>${row [1]}</td>
       </tr>`;
    })//нужно создать массив row?
    return xxx;
    
    // Пишите код решения здесь
- }
+}
 
-
- function getDropdown(currencies) {
+//?Пример из другого проекта
+/*function getDropdown(currencies) {
    console.log(currencies);
    let value = '<option value="">Выберите валюту</option>';
    currencies.forEach(function(currency) {
       value += `<option value="${currency}">${currency}</option>`;
    })
    return value;// Пишите код решения здесь
-}
+}*/
 
 
 
-const html = renderTableRows(data)
+const html = renderTableRows(data);
+const html1 = renderTableRows(data1);
 
-const tbody = document.querySelector("#prices-table tbody")
-tbody.insertAdjacentHTML("beforeend", html)
+const tbody = document.querySelector("#prices-table tbody");
+tbody.insertAdjacentHTML("beforeend", html);
+
+const tbody1 = document.querySelector("#prices-table tbody");
+tbody1.insertAdjacentHTML("beforeend", html1);
 
 
