dc49279860d2805b035cbf1da67c252485cd99fd
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2022-12-01 05:33:30 +0300

    Update_09

---
+++ JAVASCRIPT/MSC/000_povtorenie_02/000_povtorenie_02.js
@@ -0,0 +1,19 @@
+
+
+let y = 1;
+let x = 3;
+function sum (y, x) {
+   return y + x;
+}
+console.log(sum(x,y));
+
+
+
+
+
+//
+//*TODO 
+//*
+//*!
+//*?
+/* ... */
---
+++ JAVASCRIPT/MSC/000_povtorenie_02/dist/000_povtorenie_02.dev.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var y = 1;
+var x = 3;
+
+function sum(y, x) {
+  return y + x;
+}
+
+console.log(sum(x, y)); //
+//*TODO 
+//*
+//*!
+//*?
+
+/* ... */
