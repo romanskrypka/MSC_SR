5a30234de429f0259fd09cdde5876fb1c59da0f6
romanskrypka <58446922+romanskrypka@users.noreply.github.com>
2023-03-07 12:36:41 +0300

    Update_10

--- JAVASCRIPT/MSC/000_povtorenie_02/000_povtorenie_02.js
+++ JAVASCRIPT/MSC/000_povtorenie_02/000_povtorenie_02.js
@@ -5,9 +5,15 @@ let x = 3;
 function sum (y, x) {
    return y + x;
 }
-console.log(sum(x,y));
+console.log(sum(x, y));
 
+const savage = 21;
 
+if (savage >= 22) {
+   console.log("Проходной балл");//Выполнится 
+} else {
+   console.log("Недобор баллов")
+}
 
 
 
--- JAVASCRIPT/MSC/000_povtorenie_02/dist/000_povtorenie_02.dev.js
+++ JAVASCRIPT/MSC/000_povtorenie_02/dist/000_povtorenie_02.dev.js
@@ -7,7 +7,14 @@ function sum(y, x) {
   return y + x;
 }
 
-console.log(sum(x, y)); //
+console.log(sum(x, y));
+var savage = 21;
+
+if (savage >= 22) {
+  console.log("Проходной балл"); //Выполнится 
+} else {
+  console.log("Недобор баллов");
+} //
 //*TODO 
 //*
 //*!
